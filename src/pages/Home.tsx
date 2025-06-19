2211281f88e782ed2c20004f203b8199a19b7578
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2025-06-19 16:48:15 -0700

    Add title+logo

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import './Home.css'
+
+export function Home() {
+    return (
+        <>
+            <div className="home--container">
+                <div className="home--row">
+                    {/* Title + Logo */}
+                    <div className="home--column">
+                        <div className='home--row'>
+                            <img src="/images/ocean-logo.svg" alt="" />
+                            <div className="title--text">Cholula Psychological</div>
+                        </div>
+                    </div>
+
+                    {/* Nav Bar */}
+                    <div className='home--column'>
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
