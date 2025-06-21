89f4d36adb4f5a671f5bd04571b19a49a56b18da
Jesus Rodriguez-Luna <112538272+JRodriguez-Luna@users.noreply.github.com>
2025-06-20 18:20:15 -0700

    Add image adjustment, text position, color, and blue effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,20 @@
+import './Header.css'
+
 export function Header() {
     return (
-        <div className="home--container">
-            <div className="home--row header">
+        <div className="header--container">
+            <div className="header--row header">
                 {/* Title + Logo */}
-                <div className="home--column">
-                    <div className='home--row logo-title'>
+                <div className="header--column">
+                    <div className='header--row logo-title'>
                         <img src="/images/ocean-logo.svg" alt="" />
                         <div className="title--text">Cholula Psychological</div>
                     </div>
                 </div>
 
                 {/* Nav Bar */}
-                <div className='home--column'>
-                    <nav className="home--row gap text-m">
+                <div className='header--column'>
+                    <nav className="header--row gap text-m">
                         <p>Home</p>
                         <p>About</p>
                         <p>Service</p>
@@ -21,7 +23,7 @@ export function Header() {
                     </nav>
                 </div>
 
-                <div className="home--column">
+                <div className="header--column">
                     <button className="button--quick-click text-m" type="button">Book Appointment</button>
                 </div>
             </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,19 @@
+import './Hero.css'
+
 export function Hero() {
-    return <div>Hero</div>
+    return (
+        <div className="hero--container">
+            <div className="img-display">
+                <img className="hero--img" src="/images/ocean-bg.svg" alt="" />
+            </div>
+            <div className="text-img hero--column-half">
+                <div className="hero--row">
+                    <h1 className='welcome-title'>Welcome to Cholula Psychological Services</h1>
+                </div>
+                <div className='hero--row'>
+                    <p>Helping you navigate life's waves with compassion and expertise</p>
+                </div>
+            </div>
+        </div>
+    )
 }
